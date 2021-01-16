dece4b15103f8f3708cedfdac9bc015108d43fe9
JP <jpsala@gmail.com>
2021-01-15 22:41:30 -0300

    Removed unused gapi from boot

--- quasar.conf.js
+++ quasar.conf.js
@@ -20,8 +20,7 @@ module.exports = function (/* ctx */) {
     // https://quasar.dev/quasar-cli/boot-files
     boot: [
       'router',
-      'icomoon',
-      'gapi'
+      'icomoon'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
