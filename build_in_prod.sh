d3a05e236755ac666d770238f419b9e347417b61
JP <jpsala@gmail.com>
2021-02-02 19:01:42 -0300

    ..

--- build_in_prod.sh
+++ build_in_prod.sh
@@ -2,4 +2,5 @@ host=slotoprizes.tagadagames.com
 git ca ..
 git push
 ssh $host 'cd /prg/portal ; git pull ; quasar build'
+ssh $host 'ln -s /var/www/html/public/assets/web_build_live /var/www/html/portal'
 echo Listo
